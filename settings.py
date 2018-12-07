f462de4d94e3dcc44c3915478c84d1246e9f4a03
Marek Chalupa <xchalup4@fi.muni.cz>
2018-12-07 15:19:48 +0100

    split off settings.py

---
+++ settings.py
@@ -0,0 +1,9 @@
+user = "xchalup4"
+archive="run.zip"
+script="run.sh"
+folder="/var/data/xchalup4/experiments/"
+
+machines = ["ben01", "ben02", "ben03", "ben04", "ben05", "ben06",
+            "ben07", "ben09", "ben12", "ben13"]
+
+
